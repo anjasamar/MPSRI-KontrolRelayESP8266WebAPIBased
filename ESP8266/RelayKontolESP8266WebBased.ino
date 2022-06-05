553017abcd6da71509549bc1e73102bc0d333b6c
Anjas amar pradana <anjastech01@gmail.com>
2022-06-06 03:10:14 +0700

    Update RelayKontolESP8266WebBased.ino

--- ESP8266/RelayKontolESP8266WebBased.ino
+++ ESP8266/RelayKontolESP8266WebBased.ino
@@ -4,7 +4,7 @@
 //Author: Anjas Amar Pradana                                                      |
 //Mail: atsidev.io@gmail.com                                                      |
 //Program khusus board: esp8266 (Nodemcu)                                         |
-//Official Sites: http://atsi.mpsri.skom.id                                       |
+//Official Sites: http://atsidev.mpsri.skom.id                                    |
 //---------------------------------------------------------------------------------
 
 //=========================================================Perpus-Fungsi======================================================================
@@ -49,7 +49,7 @@ const char* namaAP = "ESP-MPSRI-SETUP"; //Ubah Nama Akses Poin Setup
 const char* passwordAP = "atsidevio"; //Ubah Sandi Akses Poin Setup
 
 //=========================================================Setup-IDBoard======================================================================
-//ID Token Board, Silakan Masuk Ke Akun Anda Atau Daftar Di http://atsi.mpsri.skom.id Untuk Membuat Dan Melihat Token Anda Sendiri
+//ID Token Board, Silakan Masuk Ke Akun Anda Atau Daftar Di http://atsidev.mpsri.skom.id Untuk Membuat Dan Melihat Token Anda Sendiri
 String board = "atsi-mpsri-token-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"; //Token Contoh: atsi-mpsri-token-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
 //==========================================================Void-Setup=========================================================================
@@ -269,5 +269,5 @@ void loop() {
 //Author: Anjas Amar Pradana                                                      |
 //Mail: atsidev.io@gmail.com                                                      |
 //Program khusus board: esp8266 (Nodemcu)                                         |
-//Official Sites: http://atsi.mpsri.skom.id                                       |
+//Official Sites: http://atsidev.mpsri.skom.id                                    |
 //---------------------------------------------------------------------------------
